e0aeff62fa7af0368a34d3628e99cb0240d4d1d0
SpidySamurai <javierchiortiz@gmail.com>
2023-06-28 12:15:28 -0600

    Add: Assets provided by evolve

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,9 +1,33 @@
+import React from 'react'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import Home from '../Home'
+import Gameboard from '../Gameboard'
+import Result from '../Result'
+import NotFound from '../NotFound'
+import Layout from '../../Components/Layout'
 import './App.css'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-100">
-      Initial commit, initial structure project
-    </h1>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={
+          <Layout>
+            <Home />
+          </Layout>
+        } />
+        <Route path='/gameboard' element={
+          <Layout>
+            <Gameboard />
+          </Layout>
+        } />
+        <Route path='/result' element={
+          <Layout>
+            <Result />
+          </Layout>
+        } />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </HashRouter>
   )
 }
