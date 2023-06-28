5cc12ab85dd942f9f12ea2e64b3a17e2ff8d71d0
SpidySamurai <javierchiortiz@gmail.com>
2023-06-28 12:16:08 -0600

    Add: Home page and animations

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg'
+
+const HomePage = () => {
+    return (
+        <div className="h-full flex flex-col items-center justify-center bg-gray-100 text-center">
+            <h1 className="text-5xl font-semibold animate-slideDown">
+                Memory Card Game{' '}
+                <img className="inline-block w-16" src={logo} alt="Logo" />
+            </h1>
+
+            <p className="text-xl text-gray-600 mt-4">Test your memory and have fun!</p>
+            <Link
+                to="/gameboard"
+                className="mt-6 inline-block px-8 py-3 text-lg font-semibold text-white bg-green-500 rounded hover:bg-green-600 animate-slideUp hover:animate-bounce"
+            >
+                Start Game
+            </Link>
+        </div>
+    );
+};
+
+export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-700%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        slideUp: {
+          '0%': { transform: 'translateY(+700%)', opacity: '0' },
+          '100%': { transform: 'translateY(0%)', opacity: '1' },
+        },
+        bounce: {
+          '0%, 100%': { transform: 'translateY(0)', animationTimingFunction: 'cubic-bezier(0.8,0,1,1)',opacity: 1 },
+          '50%': { transform: 'translateY(-25%)', animationTimingFunction: 'cubic-bezier(0,0,1,1)' ,opacity: .9 },
+        },
+    
+      },
+      animation: {
+        slideDown: 'slideDown 1s ease-in-out',
+        slideUp: 'slideUp 1s ease-in-out forwards',
+        bounce: 'slideUp 1s 1,bounce 2s infinite linear ',
+      },
+    },
   },
   plugins: [],
 }
